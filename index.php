f1aa5c031cb71ec222c0439e471f6bf1bca7ce70
Yannick <yannick.huet@student.graafschapcollege.nl>
2024-03-13 13:07:20 +0100

    commentaar

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=\, initial-scale=1.0">
+    <title>Git Oefen Opdracht</title>
+</head>
+<body>
+<div class="container">
+
+
+    
+    <div class="jumbotron">
+    <h1>Git Oefenopdracht</h1><br>
+   
+    <h2>
+        5-3-2024
+        9:40
+    </h2><br>
+
+    <p>
+        Yannick en steyn
+    </p>
+
+    </div>
+    <img src="img/bayern.jpg" class="img-rounded" alt="Git Deployment">
+</div>
+</body>
+</html>
